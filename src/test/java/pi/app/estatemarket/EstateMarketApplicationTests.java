54c4f345d776b10fa261627ac0516122988b718a
Sami BEN FADHL <samibenfadhl@gmail.com>
2023-02-11 16:50:40 +0100

    creation

---
+++ src/test/java/pi/app/estatemarket/EstateMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.app.estatemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
